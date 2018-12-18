7d62666893af65a53e12496bc91271c5f2cd9ee0
felipedse <felipedse@gmail.com>
2018-12-18 17:09:20 -0200

    Update xTableBox.js

--- xTableBox.js
+++ xTableBox.js
@@ -1,10 +1,94 @@
-define( ["qlik", "jquery", "text!./style.css","core.models/engine", "util"], function (qlik, $, cssContent, engine, util) {
+var lista_tabelas_array=[];
+
+define( ["qlik", "jquery", "text!./style.css", "util"], function (qlik, $, cssContent, engine, util) {
 	'use strict';
 	
   	$( "<style>" ).html( cssContent ).appendTo( "head" );
-  
-  	var app = engine.currentApp;
-  
+  	var app =qlik.currApp(this);
+  	//var app = engine.currentApp;
+
+
+  	app.createCube({
+	"qInitialDataFetch": [
+		{
+			"qHeight": 1000,
+			"qWidth": 6
+		}
+	],
+
+
+	"qDimensions": [
+		{
+			"qDef": {
+				"qFieldDefs": [
+					"=[$Table]"
+				]
+			},
+			"qNullSuppression": false,
+			"qOtherTotalSpec": {
+				"qOtherMode": "OTHER_OFF",
+				"qSuppressOther": false,
+				"qOtherSortMode": "OTHER_SORT_DESCENDING",
+				"qOtherCounted": {
+					"qv": "5"
+				},
+				"qOtherLimitMode": "OTHER_GE_LIMIT"
+			}
+		},
+		{
+			"qDef": {
+				"qFieldDefs": [
+					"=[$Field]"
+				]
+			},
+			"qNullSuppression": false,
+			"qOtherTotalSpec": {
+				"qOtherMode": "OTHER_OFF",
+				"qSuppressOther": false,
+				"qOtherSortMode": "OTHER_SORT_DESCENDING",
+				"qOtherCounted": {
+					"qv": "5"
+				},
+				"qOtherLimitMode": "OTHER_GE_LIMIT"
+			}
+		}
+	],
+	
+	"qMeasures": [
+		{
+			"qDef": {
+				"qDef": "=1"
+			},
+			"qLabel": "tabelas",
+			"qLibraryId": null,
+			"qSortBy": {
+				"qSortByState": 0,
+				"qSortByFrequency": 0,
+				"qSortByNumeric": 0,
+				"qSortByAscii": 0,
+				"qSortByLoadOrder": -1,
+				"qSortByExpression": 0,
+				"qExpression": {
+					"qv": " "
+				}
+			}
+		}
+	],
+	"qSuppressZero": false,
+	"qSuppressMissing": false,
+	"qMode": "S",
+	"qInterColumnSortOrder": [],
+	"qStateName": "$"
+},teste);
+function teste(reply, app){
+	var lista_tabelas =reply.qHyperCube.qDataPages[0].qMatrix;
+	lista_tabelas_array=[];
+	$.each(lista_tabelas, function(index, value) {
+		lista_tabelas_array.push({'Table':this[0].qText , 'Field':this[1].qText});
+
+	});
+
+ }
   	var qWindowSize = {
 			qcx : 1000,
 			qcy : 1000
@@ -215,11 +299,8 @@ define( ["qlik", "jquery", "text!./style.css","core.models/engine", "util"], fun
 			}
 
 		  
-			// Populate the dropdown.		  
-		  	app.getTablesAndKeys(qWindowSize, qNullSize, 0, false, false).then(function(tables) {
-				
-		  		qtr = tables.qtr;
-			  	$('#tableSelector_'+id)
+			// Populate the dropdown.	
+			$('#tableSelector_'+id)
 						.find('option')
     					.remove()
     					.end()
@@ -227,15 +308,21 @@ define( ["qlik", "jquery", "text!./style.css","core.models/engine", "util"], fun
     						value: '-',
     						text: '-'
 						}));
+			$.each(lista_tabelas_array, function(key, table) { 
+
+		  	//app.getTablesAndKeys(qWindowSize, qNullSize, 0, false, false).then(function(tables) {
+				
+		  		
+			  	
 			  
-				$.each(qtr, function(key, table) {   
+				
 		   			$('#tableSelector_'+id)
-			   			.append($("<option></option>")
-			   			.attr("value",table.qName)
-						.prop("selected", (selectedTable == table.qName ? true : false))
-			   			.text(table.qName)); 
+			   			.append($("<option></option>"))
+			   			.attr("value",table.Table)
+						.prop("selected", (selectedTable == table.Table ? true : false))
+			   			.text(table.Table); 
 	  			});
-	  		});
+	  		
 		  
 		  
 		  	// Handle drag and select
@@ -324,11 +411,11 @@ define( ["qlik", "jquery", "text!./style.css","core.models/engine", "util"], fun
 				  		console.log("japp");
 						//var tmpDimension = $.extend(true,{},reply.qHyperCubeDef.qDimensions[0]);
 						var dimensions = [];
-						$.each(qtr, function(key, table) {  
-							if(table.qName == selectedTable) {
-								reply.qHyperCubeDef.qInitialDataFetch[0].qWidth = table.qFields.length;
+						$.each(lista_tabelas_array, function(key, table) {  
+							if(table.Table == selectedTable) {
+								reply.qHyperCubeDef.qInitialDataFetch[0].qWidth = 1000;
 					  		
-								$.each(table.qFields, function(key, column) {
+								$.each(table.Field, function(key, column) {
 									var tmpDimension = JSON.parse('{"qDef":{"qGrouping":"N","qFieldDefs":"","qFieldLabels":[""],"qSortCriterias":[{"qSortByAscii":1,"qSortByLoadOrder":1,"qExpression":{}}],"qNumberPresentations":[],"qActiveField":0,"autoSort":true,"cId":"","othersLabel":"Övriga"},"qOtherTotalSpec":{"qOtherMode":"OTHER_OFF","qOtherCounted":{"qv":"10"},"qOtherLimit":{"qv":"0"},"qOtherLimitMode":"OTHER_GE_LIMIT","qForceBadValueKeeping":true,"qApplyEvenWhenPossiblyWrongResult":true,"qOtherSortMode":"OTHER_SORT_DESCENDING","qTotalMode":"TOTAL_OFF","qReferencedExpression":{}},"qOtherLabel":{},"qTotalLabel":{},"qCalcCond":{}}');
 								  	tmpDimension.qDef.cId = util.generateId();
 									tmpDimension.qDef.qFieldDefs = [column.qName];
